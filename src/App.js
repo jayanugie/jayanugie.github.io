b6b2146166df1faf3319ef0c9c8827bc8ca010c3
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-10-13 19:59:16 +0700

    user tailwind

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hallo, I am Nugie
-        </a>
+        <div>
+          <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+            <img
+              class="w-24 h-24 md:w-48 md:h-auto rounded-lg mx-auto object-cover"
+              src="/profile.png"
+              alt=""
+              width="384"
+              height="512"
+            />
+            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+              <blockquote>
+                <p class="text-lg font-medium">
+                  “Lorem Ipsum is simply dummy text of the printing and typesetting industry.”
+                </p>
+              </blockquote>
+              <figcaption class="font-medium">
+                <div class="text-sky-500 dark:text-sky-400">
+                  Nugie Jaya Nugraha
+                </div>
+                <div class="text-slate-700 dark:text-slate-500">
+                  Frontend Developer
+                </div>
+              </figcaption>
+            </div>
+          </figure>{" "}
+        </div>
       </header>
     </div>
   );
