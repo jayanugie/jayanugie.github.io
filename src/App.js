273bc21c236f955408e3e2da2567a1b1dbf57b77
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-10-31 23:55:07 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ function App() {
   return (
     <div className="text-white">
       {/* navbar */}
-      <div className="border border-sky-500 p-4">
-        <div className="border border-white container mx-auto p-1 flex flex-row text-center">
-          <div className="font-bold border border-yellow-50 basis-1/4">
+      <div className="border border-sky-500 p-3">
+        <div className="container mx-auto p-1 flex flex-row text-center">
+          <div className="font-bold basis-2/12">
             jayanugie
           </div>
-          <div className="border border-red-50 basis-1/4">lorem</div>
-          <div className="border border-red-500 basis-1/2">ipsum</div>
+          <div className=" basis-4/6">
+            <div className="flex flex-row">
+              <div className="basis-1/4">home</div>
+              <div className="basis-1/4">about me</div>
+              <div className="basis-1/4">projects</div>
+              <div className="basis-1/4">contact</div>
+            </div>
+          </div>
+          <div className=" basis-2/12">whasap</div>
         </div>
       </div>
     </div>
