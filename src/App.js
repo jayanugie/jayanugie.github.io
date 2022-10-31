866f207e850dc78db67eb99ae7bd30773a991025
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-10-31 23:30:28 +0700

    initial commit: start building a landing page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGit, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-            <img
-              class="w-24 h-24 md:w-48 md:h-auto rounded-lg mx-auto object-cover"
-              src="/profile.png"
-              alt=""
-              width="384"
-              height="512"
-            />
-            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-              <figcaption class="font-medium">
-                <div class="text-sky-500 dark:text-sky-400">
-                  Nugie Jaya Nugraha
-                </div>
-                <div class="text-slate-700 dark:text-slate-500">
-                  Frontend Developer
-                </div>
-                <section className="mt-5 flex space-x-2">
-                  <a className="border rounded px-2" href="https://www.github.com/jayanugie" target='blank'>
-                    <FontAwesomeIcon icon={faGit} />
-                  </a>
-                  <a className="border rounded px-2" href="https://www.linkedin.com/in/jayanugie" target='blank'>
-                    <FontAwesomeIcon icon={faLinkedin} />
-                  </a>
-                </section>
-              </figcaption>
-            </div>
-          </figure>{" "}
+    <div className="text-white">
+      {/* navbar */}
+      <div className="border border-sky-500 p-4">
+        <div className="border border-white container mx-auto p-1 flex flex-row text-center">
+          <div className="font-bold border border-yellow-50 basis-1/4">
+            jayanugie
+          </div>
+          <div className="border border-red-50 basis-1/4">lorem</div>
+          <div className="border border-red-500 basis-1/2">ipsum</div>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
