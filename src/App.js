6808d41fb0e16a59c1c236bd6c10399f2fe956d1
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-11-01 21:51:56 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 
 function App() {
   return (
     <div className="text-white">
       {/* navbar */}
-      <div className="border border-sky-500 p-3">
-        <div className="container mx-auto p-1 flex flex-row text-center">
-          <div className="font-bold basis-2/12">
-            jayanugie
-          </div>
-          <div className=" basis-4/6">
-            <div className="flex flex-row">
-              <div className="basis-1/4">home</div>
-              <div className="basis-1/4">about me</div>
-              <div className="basis-1/4">projects</div>
-              <div className="basis-1/4">contact</div>
+      <div className="p-3 bg-sky-900">
+        <div className="container mx-auto p-1 flex flex-row">
+          <div className="basis-2/6">
+            <div className="flex username">
+              <img src="/profile.jpeg" alt="profile" className="rounded-full mx-3 bg-auto"/>
+              <p>jayanugie</p>
             </div>
           </div>
-          <div className=" basis-2/12">whasap</div>
+          <div className="basis-2/6">
+            <div className="md:flex flex-row text-center hidden">
+              <div className="basis-1/4">
+                <a className="hover:text-sky-500" href="#home">
+                  Home
+                </a>
+              </div>
+              <div className="basis-1/4">
+                <a className="hover:text-sky-500" href="#about">
+                  About
+                </a>
+              </div>
+              <div className="basis-1/4">
+                <a className="hover:text-sky-500" href="#projects">
+                  Projects
+                </a>
+              </div>
+              <div className="basis-1/4">
+                <a className="hover:text-sky-500" href="#contact">
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="basis-2/6 text-right mx-3">
+            <a href="#whwatsapp">
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
