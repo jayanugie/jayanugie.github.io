1786f540845639740b0d85d0fc2c3a59a01a3bb0
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-03-03 11:19:30 +0700

    delete unnecessary

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1 class="text-3xl font-bold underline flex justify-center">
+      HELLO WORLD
+    </h1>
+  </div>
 </template>
+
+<script></script>
