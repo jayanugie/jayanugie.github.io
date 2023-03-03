98b29b512c587431baab8a4d91d68e9f40b787cf
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-03-03 10:40:32 +0700

    change to vuejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 };
