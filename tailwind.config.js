ffee5515d5b8e93cfc3c99af7532460ce79fa839
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-03-03 11:33:24 +0700

    set theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-primary": "#161616",
+        "bg-navbar": "#1c1c1c",
+        "hover-navbar": "#2b2b2b",
+        "text-primary": "#f2f2f2",
+        "text-secondary": "#7b7b7b",
+      },
+    },
   },
   plugins: [],
 };
