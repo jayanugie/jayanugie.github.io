2f773d6375f5a4af6ee594d87497bb0eedd2b520
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-03-13 17:19:55 +0700

    add whats new

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="sm:ml-64 h-screen">
+  <div class="sm:ml-64 pb-60">
     <div class="xl:px-80 lg:px-32 md:px-28 px-10 md:pt-24 pt-10">
+      <!-- Introduction Text -->
       <div class="text-text-primary space-y-4">
         <p class="text-4xl font-semibold">
           Hey, I'm Nugie <br />
           I'm <span class="text-text-secondary">Web Developer</span>
         </p>
-        <p>
+        <p class="font-medium">
           I am an experienced web developer based in Jakarta, Indonesia. Ready
           to help find a solution for you.
         </p>
-        <div class="space-x-4 flex">
+        <div class="space-x-4 flex font-medium">
           <button
             class="pt-1 pb-2 px-3 border border-border-color bg-hover-navbar rounded-lg"
           >
@@ -24,6 +25,60 @@
               <span>Email</span>
             </div>
           </button>
+        </div>
+      </div>
+
+      <!-- What's new -->
+      <div class="mt-12 space-y-4">
+        <p class="text-text-primary font-medium p-1">What's new</p>
+
+        <!-- Content 1 -->
+        <div
+          class="flex md:p-3 p-2 rounded-lg hover:bg-hover-navbar transition-all"
+        >
+          <img
+            src="/img/phone.jpg"
+            alt="phone"
+            class="md:w-56 w-32 rounded-lg object-cover"
+          />
+          <div class="text-sm ml-4 space-y-2">
+            <p class="text-text-primary font-medium">CyberFolio</p>
+            <p class="text-text-secondary">
+              A beautiful dark-themed personal website template.
+            </p>
+          </div>
+        </div>
+        <!-- Content 2 -->
+        <div
+          class="flex md:p-3 p-2 rounded-lg hover:bg-hover-navbar transition-all"
+        >
+          <img
+            src="/img/phone.jpg"
+            alt="phone"
+            class="md:w-56 w-32 rounded-lg object-cover"
+          />
+          <div class="text-sm ml-4 space-y-2">
+            <p class="text-text-primary font-medium">CyberFolio</p>
+            <p class="text-text-secondary">
+              A beautiful dark-themed personal website template.
+            </p>
+          </div>
+        </div>
+        <!-- Content 3 -->
+        <div
+          class="flex md:p-3 p-2 rounded-lg hover:bg-hover-navbar transition-all"
+        >
+          <img
+            src="/img/phone.jpg"
+            alt="phone"
+            class="md:w-56 w-32 rounded-lg object-cover"
+          />
+          <div class="text-sm ml-4 space-y-2">
+            <p class="text-text-primary font-medium">CyberFolio</p>
+            <p class="text-text-secondary">
+              A beautiful dark-themed personal website template.
+            </p>
+          </div>
         </div>
       </div>
     </div>
