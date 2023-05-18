125f5e8def8c71fa9e8afddbec0828b7ffd01cc5
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-05-18 19:35:24 +0700

    trigger

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import ContactView from "../views/ContactView.vue";
@@ -8,7 +8,7 @@ import ProjectView from "../views/ProjectView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
